9d97c528c5713a14caf7842e8df693ba8f1ad049
DESKTOP-7CB8L9L\ASUS <alex.svyridov@vitalerter.com>
2021-07-28 23:19:13 +0300

    changed io on clien

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import './App.scss';
 import { Hello } from './components/hello';
-
+import io from 'socket.io-client'
 function App() {
+  const socket = io();
+
+  socket.on('chat message', (msg) => {
+    window.scrollTo(0, document.body.scrollHeight)
+  })
   return (
     <div className="App">
       <Hello />
