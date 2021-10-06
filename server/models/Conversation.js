0dd0ea5c6280deb1112280a9ebd3971a4a883afb
DESKTOP-7CB8L9L\ASUS <alex.svyridov@vitalerter.com>
2021-10-06 15:55:25 +0300

    1

--- server/models/Conversation.js
+++ server/models/Conversation.js
@@ -3,7 +3,8 @@ const  mongoose =  require('mongoose')
 const ConversationSchema = new mongoose.Schema(
     {
         members:Array
-    }
+    },
+    {timestamps: true}
 )
 
 module.exports = mongoose.model("Conversation", ConversationSchema);
